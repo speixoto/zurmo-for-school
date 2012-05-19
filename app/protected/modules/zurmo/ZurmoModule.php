283475ed170d0fbbf79463c6accf2a344469bc29
digimagic <none@none>
2012-05-19 08:53:18 -0500

    Fixing issue with auditEvent serializedData not correctly saving as a text column during installation or update.

--- app/protected/modules/zurmo/ZurmoModule.php
+++ app/protected/modules/zurmo/ZurmoModule.php
@@ -55,7 +55,7 @@
             // modules, and because ZurmoModule is the root of the module
             // dependence hierarchy it needed concern itself, other than
             // with the models that are specific to itself.
-            return array('NamedSecurableItem', 'GlobalMetadata', 'PerUserMetadata', 'Portlet', 'CustomFieldData',
+            return array('AuditEvent', 'NamedSecurableItem', 'GlobalMetadata', 'PerUserMetadata', 'Portlet', 'CustomFieldData',
                          'CalculatedDerivedAttributeMetadata', 'DropDownDependencyDerivedAttributeMetadata');
         }
 
