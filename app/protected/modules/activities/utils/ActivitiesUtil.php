2bcf28da88fc3677b14e748e6917da1d170bdaba
digimagic <none@none>
2012-05-22 15:11:42 -0500

    Work on cleaning up latest activities summary view column details.
    --HG--
    branch : userinterface2

--- app/protected/modules/activities/utils/ActivitiesUtil.php
+++ app/protected/modules/activities/utils/ActivitiesUtil.php
@@ -51,14 +51,44 @@
             return array_reverse($modelDerivationPathToItem);
         }
 
-        public static function renderSummaryContent(RedBeanModel $model, $redirectUrl)
+        /**
+         * Renders and returns string content of summary content for the given model.
+         * @param RedBeanModel $model
+         * @param mixed $redirectUrl
+         * @param string $ownedByFilter
+         * @param string $viewModuleClassName
+         * @return string content
+         */
+        public static function renderSummaryContent(RedBeanModel $model, $redirectUrl, $ownedByFilter, $viewModuleClassName)
         {
-            $mashableActivityRules = MashableActivityRulesFactory::createMashableActivityRulesByModel(
-                                         get_class($model));
-            $orderByAttributeName = $mashableActivityRules->getLatestActivitiesOrderByAttributeName();
+            assert('is_string($redirectUrl) || $redirectUrl == null');
+            assert('is_string($ownedByFilter)');
+            assert('is_string($viewModuleClassName)');
+            $mashableActivityRules  = MashableActivityRulesFactory::createMashableActivityRulesByModel(get_class($model));
+            $orderByAttributeName   = $mashableActivityRules->getLatestActivitiesOrderByAttributeName();
+            $summaryContentTemplate = $mashableActivityRules->getSummaryContentTemplate($ownedByFilter, $viewModuleClassName);
+
+            //Render icon
             $content  = '<span class="'.get_class($model).'"></span>';
+            //Render date
             $content .= '<strong>'.DateTimeUtil::convertDbFormattedDateTimeToLocaleFormattedDisplay(
                             $model->{$orderByAttributeName}, 'long', null) . '</strong><br/>';
+
+            $data                                            = array();
+            $data['modelStringContent']                      = self::renderModelStringContent($model, $redirectUrl);
+            $data['ownerStringContent']                      = self::renderOwnerStringContent($model);
+            $data['relatedModelsByImportanceContent']        = self::renderRelatedModelsByImportanceContent($model);
+            $data['extraContent']                            = self::resolveAndRenderExtraContent($model,
+                                                                     $mashableActivityRules);
+
+            //Render display content
+            $content .= self::resolveContentTemplate($summaryContentTemplate, $data);
+            return $content;
+        }
+
+        protected static function renderModelStringContent(RedBeanModel $model, $redirectUrl)
+        {
+            assert('is_string($redirectUrl) || $redirectUrl == null');
             $modelDisplayString = strval($model);
             if (strlen($modelDisplayString) > 200)
             {
@@ -68,18 +98,143 @@
             {
                 $modelDisplayString = '<span style="text-decoration:line-through;">' . $modelDisplayString . '</span>';
             }
-            $params = array('label' => $modelDisplayString, 'redirectUrl' => $redirectUrl);
+            $params          = array('label' => $modelDisplayString, 'redirectUrl' => $redirectUrl);
             $moduleClassName = $model->getModuleClassName();
             $moduleId        = $moduleClassName::getDirectoryName();
             $element  = new DetailsLinkActionElement('default', $moduleId, $model->id, $params);
-            $content .= $element->render() . '<br/>';
-            //$content .= Yii::t('Default', 'by') . '&#160;' . Yii::app()->format->text($model->createdByUser);
+            return $element->render();
+        }
+
+        protected static function renderOwnerStringContent($model)
+        {
+            if($model instanceof MashableActivity)
+            {
+                return strval($model->owner);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /**
+         * Renders related models. But only renders one type of related model given that the $model supplied
+         * is connected to more than one type of activity item.  There is an order of importance that is checked
+         * starting with Account, then Contact, then Opportunity. If none are found, then it grabs the first available.
+         * @see getActivityItemsStringContentByModelClassName
+         * @param RedBeanModel $model
+         */
+        protected static function renderRelatedModelsByImportanceContent(RedBeanModel $model)
+        {
+            if($model->activityItems->count() == 0)
+            {
+                return;
+            }
+            $stringContent = self::getActivityItemsStringContentByModelClassName($model, 'Account');
+            if($stringContent != null)
+            {
+                return Yii::t('Default', 'for {relatedModelsStringContent}', array('{relatedModelsStringContent}' => $stringContent));
+            }
+            $stringContent = self::getActivityItemsStringContentByModelClassName($model, 'Contact');
+            if($stringContent != null)
+            {
+                return Yii::t('Default', 'with {relatedContactsStringContent}', array('{relatedContactsStringContent}' => $stringContent));
+            }
+            $stringContent = self::getActivityItemsStringContentByModelClassName($model, 'Opportunity');
+            if($stringContent != null)
+            {
+                return Yii::t('Default', 'for {relatedModelsStringContent}', array('{relatedModelsStringContent}' => $stringContent));
+            }
+            $metadata      = Activity::getMetadata();
+            $stringContent =  self::getFirstActivityItemStringContent($metadata['Activity']['activityItemsModelClassNames'], $model);
+            if($stringContent != null)
+            {
+                return Yii::t('Default', 'for {relatedModelsStringContent}', array('{relatedModelsStringContent}' => $stringContent));
+            }
+        }
+
+        protected static function getActivityItemsStringContentByModelClassName(RedBeanModel $model, $castDownModelClassName)
+        {
+            assert('is_string($castDownModelClassName)');
+            $existingModels = array();
+            $modelDerivationPathToItem = ActivitiesUtil::getModelDerivationPathToItem($castDownModelClassName);
+            foreach ($model->activityItems as $item)
+            {
+                try
+                {
+                    $castedDownmodel = $item->castDown(array($modelDerivationPathToItem));
+                    if (get_class($castedDownmodel) == $castDownModelClassName)
+                    {
+                        $existingModels[] = strval($castedDownmodel);
+                    }
+                }
+                catch (NotFoundException $e)
+                {
+                    //do nothing
+                }
+            }
+            return self::resolveStringValueModelsDataToStringContent($existingModels);
+        }
+
+        protected static function resolveStringValueModelsDataToStringContent($modelsAndStringData)
+        {
+            assert('is_array($modelsAndStringData)');
+            $content = null;
+            foreach($modelsAndStringData as $modelStringContent)
+            {
+                if($content != null)
+                {
+                    $content .= ', ';
+                }
+                $content .= $modelStringContent;
+            }
+            return $content;
+        }
+
+        protected static function getFirstActivityItemStringContent($relationModelClassNames, RedBeanModel $model)
+        {
+            assert('is_string($relationModelClassNames)');
+            foreach ($relationModelClassNames as $relationModelClassName)
+            {
+                //ASSUMES ONLY A SINGLE ATTACHED ACTIVITYITEM PER RELATION TYPE.
+                foreach ($model->activityItems as $item)
+                {
+                    try
+                    {
+                        $modelDerivationPathToItem = ActivitiesUtil::getModelDerivationPathToItem($relationModelClassName);
+                        $castedDownModel = $item->castDown(array($modelDerivationPathToItem));
+                        return strval($castedDownModel);
+                    }
+                    catch (NotFoundException $e)
+                    {
+                        //do nothing
+                    }
+                }
+            }
+        }
+
+        protected static function resolveAndRenderExtraContent(RedBeanModel $model,
+                                                               MashableActivityRules $mashableActivityRules)
+        {
+            $content      = null;
             $extraContent = $mashableActivityRules->getLatestActivityExtraDisplayStringByModel($model);
             if ($extraContent)
             {
                 $content .= '<br/>' . $extraContent;
             }
             return $content;
+        }
+
+        protected static function resolveContentTemplate($template, $data)
+        {
+            assert('is_string($template)');
+            assert('is_array($data)');
+            $preparedContent = array();
+            foreach ($data as $templateVar => $content)
+            {
+                $preparedContent["{" . $templateVar . "}"] = $content;
+            }
+            return strtr($template, $preparedContent);
         }
 
         public static function getActivityItemsModelClassNamesDataExcludingContacts()
