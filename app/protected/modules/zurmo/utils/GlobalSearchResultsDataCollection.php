5110676ce90fb1e9ffd7ed321636c8d428c69ed1
digimagic <none@none>
2012-07-31 12:13:44 -0500

    Fixing sort issue.
    --HG--
    branch : globalsearchlist

--- app/protected/modules/zurmo/utils/GlobalSearchResultsDataCollection.php
+++ app/protected/modules/zurmo/utils/GlobalSearchResultsDataCollection.php
@@ -3,17 +3,17 @@
  * GlobalSearchResultsDataCollection
  * @param
  */
-    class GlobalSearchResultsDataCollection 
-    {        
-        
-        private $term;       
-        private $scopeData;        
-        private $user;        
-        private $views = array();        
-        
+    class GlobalSearchResultsDataCollection
+    {
+
+        private $term;
+        private $scopeData;
+        private $user;
+        private $views = array();
+
         /*
-         * @param   string      
-         * @param   integer     
+         * @param   string
+         * @param   integer
          * @param   User        User model
          * @param   array       Modules to be searched
          */
@@ -23,59 +23,65 @@
             $this->pageSize = $pageSize;
             $this->user = $user;
             $this->scopeData = $scopeData;
-            $this->makeViews();            
+            $this->makeViews();
         }
-              
+
         /*
          * makeViews
          * @return  array   moduleName => listView
          */
         private function makeViews()
-        {                                                
+        {
             $pageSize = $this->pageSize;
             $globalSearchModuleNamesAndLabelsData = GlobalSearchUtil::
-                    getGlobalSearchScopingModuleNamesAndLabelsDataByUser($this->user);            
+                    getGlobalSearchScopingModuleNamesAndLabelsDataByUser($this->user);
             foreach ($globalSearchModuleNamesAndLabelsData as $moduleName => $label)
-            {        
+            {
                 if ($this->scopeData == null || in_array($moduleName, $this->scopeData))
-                {                        
-                    $module = Yii::app()->findModule($moduleName);                
-                    $searchFormClassName = $module::getGlobalSearchFormClassName();
-                    $modelClassName = $module::getPrimaryModelName();                
-                    $model  = new $modelClassName(false);                
-                    $searchForm = new $searchFormClassName($model);  
-                    $sanitizedSearchAttributes = MixedTermSearchUtil::
-                            getGlobalSearchAttributeByModuleAndPartialTerm($module, $this->term);                                                
-                    $metadataAdapter = new SearchDataProviderMetadataAdapter(
-                            $searchForm,
-                            $this->user->id,
-                            $sanitizedSearchAttributes
-                        );                   
-                    $listViewClassName = $module::getPluralCamelCasedName() . 'ListView';                
-                    $dataProvider = RedBeanModelDataProviderUtil::makeDataProvider(
-                             $metadataAdapter->getAdaptedMetadata(false),
-                             $modelClassName,
-                             'RedBeanModelDataProvider',
-                             'createdDateTime',
-                             true,
-                             $pageSize,
-                             $module->getStateMetadataAdapterClassName()
-                        );                             
-                    $listView = new $listViewClassName(
-                            'default',
-                            $module->getId(),
-                            $modelClassName,
-                            $dataProvider,
-                            GetUtil::resolveSelectedIdsFromGet(),
-                            '-' .$moduleName);                                               
-                    $this->views[$moduleName] = $listView;
+                {
+                    if($moduleName == 'contacts')
+                    {
+                        $module = Yii::app()->findModule($moduleName);
+                        $searchFormClassName = $module::getGlobalSearchFormClassName();
+                        $modelClassName = $module::getPrimaryModelName();
+                        $model  = new $modelClassName(false);
+                        $searchForm = new $searchFormClassName($model);
+                        $sanitizedSearchAttributes = MixedTermSearchUtil::
+                                getGlobalSearchAttributeByModuleAndPartialTerm($module, $this->term);
+                        $metadataAdapter = new SearchDataProviderMetadataAdapter(
+                                $searchForm,
+                                $this->user->id,
+                                $sanitizedSearchAttributes
+                            );
+                        $listViewClassName = $module::getPluralCamelCasedName() . 'ListView';
+                        $sortAttribute     = SearchUtil::resolveSortAttributeFromGetArray($modelClassName);
+                        $sortDescending    = SearchUtil::resolveSortDescendingFromGetArray($modelClassName);
+                        $dataProvider = RedBeanModelDataProviderUtil::makeDataProvider(
+                                 $metadataAdapter->getAdaptedMetadata(false),
+                                 $modelClassName,
+                                 'RedBeanModelDataProvider',
+                                 $sortAttribute,
+                                 $sortDescending,
+                                 $pageSize,
+                                 $module->getStateMetadataAdapterClassName()
+                            );
+                        $listView = new $listViewClassName(
+                                'default',
+                                $module->getId(),
+                                $modelClassName,
+                                $dataProvider,
+                                GetUtil::resolveSelectedIdsFromGet(),
+                                '-' .$moduleName
+                                );
+                        $this->views[$moduleName] = $listView;
+                    }
                 }
             }
         }
-        
+
         public function getViews()
         {
             return $this->views;
-        }              
+        }
     }
 ?>
