b117e55e6cfa38cbf0fe8bc9a1ba3f8a7981e12e
digimagic <none@none>
2012-08-02 17:10:43 -0500

    Fixing removal of colspan tags
    --HG--
    branch : searchrefactor

--- app/protected/extensions/zurmoinc/framework/elements/Element.php
+++ app/protected/extensions/zurmoinc/framework/elements/Element.php
@@ -37,11 +37,8 @@
         protected $attribute;
         protected $form;
         protected $params;
-        //public $editableTemplate = '<th>{label}</th><td colspan="{colspan}">{content}{error}</td>';
-        //public $nonEditableTemplate = '<th>{label}</th><td colspan="{colspan}">{content}</td>';
-        
-        public $editableTemplate = '<th>{label}</th><td>{content}{error}</td>';
-        public $nonEditableTemplate = '<th>{label}</th><td>{content}</td>';
+        public $editableTemplate = '<th>{label}</th><td colspan="{colspan}">{content}{error}</td>';
+        public $nonEditableTemplate = '<th>{label}</th><td colspan="{colspan}">{content}</td>';
 
         /**
          * Constructs the element specifying the model and attribute.
