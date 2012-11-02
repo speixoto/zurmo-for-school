5b6b46e483d9d430f4e52f4da7ec571050ee2bcf
Amit Ashckenazi <obviocity@gmail.com>
2012-11-02 17:29:28 -0300

    jnotify notifications styling and markup. upgraded less.js to 1.4 which includes ++ syntax - in prep for refacroring.

--- app/protected/modules/zurmo/views/FlashMessageView.php
+++ app/protected/modules/zurmo/views/FlashMessageView.php
@@ -35,7 +35,7 @@
 
         protected function renderContent()
         {
-            $content = '<div id = "FlashMessageBar"></div>';
+            $content = '<div id="FlashMessageBar"></div>';
             if (Yii::app()->user->hasFlash('notification'))
             {
                 $script = "
