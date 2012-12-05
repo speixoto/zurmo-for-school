3cfb2c532a3851768cd4514a82ab29aec9dc0e24
ant@ant-PC <ant@ant-PC>
2012-12-05 22:57:53 +0530

    commit for massedit dates for opportunities with proper intendation

--- app/protected/modules/opportunities/tests/unit/walkthrough/OpportunitiesSuperUserWalkthroughTest.php
+++ app/protected/modules/opportunities/tests/unit/walkthrough/OpportunitiesSuperUserWalkthroughTest.php
@@ -238,6 +238,7 @@
 			
             //save Model MassEdit for selected Ids
             //Test that the 2 contacts do not have the closed date populating them with.
+			//Test that closed dates are properly updated
             $opportunity1 = Opportunity::getById($superOpportunityId);
             $opportunity2 = Opportunity::getById($superOpportunityId2);
             $opportunity3 = Opportunity::getById($superOpportunityId3);
@@ -258,7 +259,7 @@
             $this->assertEquals(5, $pageSize);
             Yii::app()->pagination->setForCurrentUserByType('massEditProgressPageSize', 20);
             $content = $this->runControllerWithRedirectExceptionAndGetContent('opportunities/default/massEdit');
-
+            
 			$opportunity1 = Opportunity::getById($superOpportunityId);
             $opportunity2 = Opportunity::getById($superOpportunityId2);
             $opportunity3 = Opportunity::getById($superOpportunityId3);
