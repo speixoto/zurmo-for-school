4e3fd0f6563758ff8e4e1e400346c740731065b1
ivica <ivica.nedeljkovic@gmail.com>
2012-12-05 09:27:12 +0100

    Fixing issue with index length for translation tables

--- app/protected/core/models/MessageSource.php
+++ app/protected/core/models/MessageSource.php
@@ -37,7 +37,7 @@
                 'rules' => array(
                     array('category',           'required'),
                     array('category',           'type', 'type' => 'string'),
-                    array('category',           'length',  'min'  => 1, 'max' => 255),
+                    array('category',           'length',  'min'  => 1, 'max' => 48),
                     array('source',             'required'),
                     array('source',             'type', 'type' => 'blob')
                 )
