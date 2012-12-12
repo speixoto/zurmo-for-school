9fc54e45406f3cd212979b9c9be476b412692e4e
Amit Ashckenazi <obviocity@gmail.com>
2012-12-12 19:04:14 -0300

    new icon for navigation. fixing dashboard column. detail view set to one column for mobile.
    --HG--
    branch : Mobile

--- app/protected/modules/zurmo/views/HeaderLinksView.php
+++ app/protected/modules/zurmo/views/HeaderLinksView.php
@@ -51,7 +51,7 @@
             $imagePath = Yii::app()->baseUrl . '/themes/default/images/';
             $homeUrl   = Yii::app()->createUrl('home/default');
             $content   = '<div class="clearfix">';
-            $content  .= '<a href="#" id="nav-trigger" title="Toggle Navigation">&#9776;</a>';
+            $content  .= '<a href="#" id="nav-trigger" title="Toggle Navigation">&rsaquo;</a>';
             $content  .= '<div id="corp-logo">';
             $content  .= '<a href="' . $homeUrl . '"><img src="' . $imagePath . 'Zurmo_logo.png" alt="Zurmo Logo"/></a>';
             if ($this->applicationName != null)
