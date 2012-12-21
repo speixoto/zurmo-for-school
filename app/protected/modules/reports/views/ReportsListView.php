e53f0d549b249c611d84e49dd0d6b4d42d8ded0f
Amit Ashckenazi <obviocity@gmail.com>
2012-12-21 19:10:45 -0300

    Adding a less file for everything related to reports/wizards styles. making report list view a SecuredListView.
    --HG--
    branch : reports

--- app/protected/modules/reports/views/ReportsListView.php
+++ app/protected/modules/reports/views/ReportsListView.php
@@ -24,7 +24,7 @@
      * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
      ********************************************************************************/
 
-    class ReportsListView extends ListView
+    class ReportsListView extends SecuredListView
     {
         public static function getDefaultMetadata()
         {
