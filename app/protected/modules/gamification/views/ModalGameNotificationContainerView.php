34621fb9e0bd3e5fe7a03cd410a5db103dfb7890
Sergey Fayngold <nbz4live@gmail.com>
2012-12-30 18:47:59 +0100

    Renamed all Yii::t() calls in protected/modules/gamification
    --HG--
    branch : newTranslateCalls

--- app/protected/modules/gamification/views/ModalGameNotificationContainerView.php
+++ app/protected/modules/gamification/views/ModalGameNotificationContainerView.php
@@ -92,13 +92,13 @@
         {
             if (!RightsUtil::canUserAccessModule('SocialItemsModule', Yii::app()->user->userModel))
             {
-                return ZurmoHtml::link(Yii::t('Default', 'Continue'), '#',
+                return ZurmoHtml::link(Zurmo::t('GamificationModule', 'Continue'), '#',
                              array('class'   => 'close-ModalGameNotification',
                                     'onclick' => '$("#ModalGameNotification' . $index . '").dialog("close");'));
             }
             else
             {
-                $content = ZurmoHtml::link(Yii::t('Default', 'Skip'), '#',
+                $content = ZurmoHtml::link(Zurmo::t('GamificationModule', 'Skip'), '#',
                                  array('class'   => 'close-ModalGameNotification simple-select',
                                        'onclick' => '$("#ModalGameNotification' . $index . '").dialog("close");'));
                 $content .= static::renderPostToProfileLinkContent($notification, $index);
@@ -116,7 +116,7 @@
             $aContent                = ZurmoHtml::tag('span', array('class' => 'z-spinner'), null);
             $aContent               .= ZurmoHtml::tag('span', array('class' => 'z-icon'),    null);
             $aContent               .= ZurmoHtml::tag('span',  array('class' => 'z-label'),
-                                                     Yii::t('Default', 'Post to Profile'));
+                                                     Zurmo::t('GamificationModule', 'Post to Profile'));
             // Begin Not Coding Standard
             $content   = ZurmoHtml::ajaxLink($aContent, $url,
                          array('type'     => 'GET',
