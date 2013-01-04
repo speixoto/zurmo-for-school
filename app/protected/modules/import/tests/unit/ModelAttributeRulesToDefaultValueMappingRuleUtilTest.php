3b00843069bdf539677b275f279f2a9cc8b2b5e1
user@user-PC <user@user-PC>
2013-01-04 12:51:50 +0530

    Reverting the Minor change

--- app/protected/modules/import/tests/unit/ModelAttributeRulesToDefaultValueMappingRuleUtilTest.php
+++ app/protected/modules/import/tests/unit/ModelAttributeRulesToDefaultValueMappingRuleUtilTest.php
@@ -107,14 +107,6 @@
                                  array('defaultValue',  'type', 'type' => 'string'),
                                  array('defaultValue',  'length',  'min'  => 2, 'max' => 32));
             $this->assertEquals($compareData, $rules);
-
-           // Testing AttributeImportRules.
-            $rules = ModelAttributeRulesToDefaultValueMappingRuleUtil::
-                     getApplicableRulesByModelClassNameAndAttributeName('ImportModelTestItem', 'lastName', 'defaultValue', true);
-            $compareData = array(array('defaultValue',  'required'),
-                                 array('defaultValue',  'type', 'type' => 'string'),
-                                 array('defaultValue',  'length',  'min'  => 2, 'max' => 32));
-            $this->assertEquals($compareData, $rules);
         }
 
         public function testGetApplicableRulesByModelClassNameAndAttributeNameForCustomCreatedTypes()
