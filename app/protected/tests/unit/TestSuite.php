6b46a5008a97dffcd3a441ba75370fd7f6d22a7d
dmilanko <none@none>
2013-01-30 22:16:42 +0100

    testing

--- app/protected/tests/unit/TestSuite.php
+++ app/protected/tests/unit/TestSuite.php
@@ -24,7 +24,7 @@
      * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
      ********************************************************************************/
    
-    $cwd = getcwd();
+    //$cwd = getcwd();
 
     require_once('../PhpUnitServiceUtil.php');
     require_once('../testRoots.php');
