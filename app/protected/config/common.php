0422aefe185d3a3669c5ff6f97b731dda2b6dca3
Sergey Fayngold <nbz4live@gmail.com>
2013-01-31 19:28:33 +0100

    Enabled the new message source
    --HG--
    branch : i18nv2

--- app/protected/config/common.php
+++ app/protected/config/common.php
@@ -86,11 +86,9 @@
             'gamificationObserver' => array(
                 'class' => 'application.modules.gamification.observers.GamificationObserver',
             ),
-            /* Will be enabled later
             'messages' => array(
                 'class' => 'application.core.components.ZurmoMessageSource',
             ),
-            */
             'minScript' => array(
                 'class' => 'application.core.components.ZurmoExtMinScript',
                 'groupMap' => array(
