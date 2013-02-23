769b87a33413631c6a33f5451f327b124621a517
digimagic <none@none>
2013-02-23 09:37:42 -0500

    Fixing issue with super user not being able to select a title

--- app/protected/modules/install/utils/InstallUtil.php
+++ app/protected/modules/install/utils/InstallUtil.php
@@ -567,7 +567,6 @@
         {
             $user = new User();
             $user->username     = $username;
-            $user->title->value = 'Mr.';
             $user->firstName    = 'Super';
             $user->lastName     = 'User';
             $user->setPassword($password);
