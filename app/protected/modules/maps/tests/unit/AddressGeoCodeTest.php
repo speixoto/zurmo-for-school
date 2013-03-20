900a45f8f19d3084a0d93339585cb8b0df41b5b0
Dhananjay Donthula <none@none>
2013-03-20 23:29:51 +0530

    maps test files removing skipping functionality

--- app/protected/modules/maps/tests/unit/AddressGeoCodeTest.php
+++ app/protected/modules/maps/tests/unit/AddressGeoCodeTest.php
@@ -42,10 +42,6 @@
 
         public function testAddressFetchLatitudeAndLongitude()
         {
-            if (Yii::app()->params['testGoogleGeoCodeApiKey'] == null)
-            {
-                $this->markTestSkipped(Zurmo::t('Default', 'Test GoogleGeoCodeApiKey is not set in perInstanceTest.php file.'));
-            }
             $super = User::getByUsername('super');
             Yii::app()->user->userModel = $super;
 
