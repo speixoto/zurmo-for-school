72f465e2455d5c6e5714ae735ffaa93b619e66e6
Amit Ashckenazi <obviocity@gmail.com>
2013-04-03 14:12:30 -0300

    show/hide next button in workflow's actions when an action is not yet validated.

--- app/protected/modules/workflows/views/OnSaveWorkflowWizardView.php
+++ app/protected/modules/workflows/views/OnSaveWorkflowWizardView.php
@@ -58,6 +58,7 @@
         {
             assert('is_string($formName)');
             return     "linkId = $('#" . $formName . "').find('.attachLoadingTarget').attr('id');
+
                         if(linkId == '" . ModuleForWorkflowWizardView::getNextPageLinkId() . "')
                         {
                             $('#" . static::getValidationScenarioInputId() . "').val('" .
@@ -77,14 +78,14 @@
                         }
                         if(linkId == '" . ActionsForWorkflowWizardView::getNextPageLinkId() . "')
                         {
-                           var actionsList = $('#ActionsForWorkflowWizardView').find('ul:first').children();
+                            /*var actionsList = $('#ActionsForWorkflowWizardView').find('ul:first').children();
                             $.each(actionsList, function(){
                                 if ( $(this).hasClass('expanded-row') ){
                                     //fires on next button when a panel is open
                                     alert('please save and validate the open action panel');
                                     return false;
                                 }
-                            });
+                            });*/
                             $('#" . static::getValidationScenarioInputId() . "').val('" .
                                 WorkflowWizardForm::EMAIL_MESSAGES_VALIDATION_SCENARIO . "');
                             $('#ActionsForWorkflowWizardView').hide();
@@ -97,13 +98,16 @@
                             $('#EmailMessagesForWorkflowWizardView').hide();
                             $('#GeneralDataForWorkflowWizardView').show();
                         }
+
                         var rowData = $('#" . $formName . "').find('.attachLoadingTarget').data() || {};
                         if (rowData.purpose === 'validate-action'){
                             $('#' + rowData.row.toString()).toggleClass('expanded-row');
                             $('#' + rowData.row.toString() + ' .toggle-me').toggle();
                             $('#' + rowData.row.toString() + ' .edit-dynamic-row-link').toggle();
                             $('#' + rowData.row.toString()).siblings().show();
+                            $('#actionsNextLink').show();
                         }
+
                         if(linkId == '" . GeneralDataForWorkflowWizardView::getNextPageLinkId() . "')
                         {
                             " . $this->getSaveAjaxString($formName) . "
