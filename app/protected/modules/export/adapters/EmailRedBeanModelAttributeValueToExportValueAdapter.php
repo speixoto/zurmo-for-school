a9f4cd32f931cbefaf0e1e1fba18de3f80aee80d
nn2 <none@none>
2013-04-04 09:27:12 +0200

    Adding tests
    --HG--
    branch : fix_email_export_header

--- app/protected/modules/export/adapters/EmailRedBeanModelAttributeValueToExportValueAdapter.php
+++ app/protected/modules/export/adapters/EmailRedBeanModelAttributeValueToExportValueAdapter.php
@@ -24,7 +24,7 @@
      * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
      ********************************************************************************/
 
-    class EmailRedBeanModelAttributeValueToExportValueAdapter extends TextRedBeanModelAttributeValueToExportValueAdapter
+    class EmailRedBeanModelAttributeValueToExportValueAdapter extends RedBeanModelAttributeValueToExportValueAdapter
     {
         public function resolveData(& $data)
         {
