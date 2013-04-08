220c94d4fb30055df6e0fe82de27e0f2f64116eb
Muhammad Shoaib <shoaibi@dotgeek.me>
2013-04-08 16:31:20 +0500

    Updated License in all files

--- app/protected/modules/reports/tests/unit/walkthrough/ReportsSuperUserWalkthroughTest.php
+++ app/protected/modules/reports/tests/unit/walkthrough/ReportsSuperUserWalkthroughTest.php
@@ -1,7 +1,7 @@
 <?php
     /*********************************************************************************
      * Zurmo is a customer relationship management program developed by
-     * Zurmo, Inc. Copyright (C) 2012 Zurmo Inc.
+     * Zurmo, Inc. Copyright (C) 2013 Zurmo Inc.
      *
      * Zurmo is free software; you can redistribute it and/or modify it under
      * the terms of the GNU General Public License version 3 as published by the
@@ -20,8 +20,18 @@
      * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
      * 02110-1301 USA.
      *
-     * You can contact Zurmo, Inc. with a mailing address at 113 McHenry Road Suite 207,
-     * Buffalo Grove, IL 60089, USA. or at email address contact@zurmo.com.
+     * You can contact Zurmo, Inc. with a mailing address at 27 North Wacker Drive
+     * Suite 370 Chicago, IL 60606. or at email address contact@zurmo.com.
+     *
+     * The interactive user interfaces in original and modified versions
+     * of this program must display Appropriate Legal Notices, as required under
+     * Section 5 of the GNU General Public License version 3.
+     *
+     * In accordance with Section 7(b) of the GNU General Public License version 3,
+     * these Appropriate Legal Notices must retain the display of the Zurmo
+     * logo and Zurmo copyright notice. If the display of the logo is not reasonably
+     * feasible for technical reasons, the Appropriate Legal Notices must display the words
+     * "Copyright Zurmo Inc. 2013. All rights reserved".
      ********************************************************************************/
 
     /**
@@ -45,27 +55,27 @@
         public function testSuperUserAllDefaultControllerActions()
         {
             $super = $this->logoutCurrentUserLoginNewUserAndGetByUsername('super');
-            
+
             $this->runControllerWithNoExceptionsAndGetContent      ('reports/default/list');
             $this->runControllerWithExitExceptionAndGetContent     ('reports/default/create');
             $this->runControllerWithNoExceptionsAndGetContent      ('reports/default/selectType');
         }
-        
+
         public function testCreateActionForRowsAndColumns()
         {
             $super = $this->logoutCurrentUserLoginNewUserAndGetByUsername('super');
-            
+
             $content = $this->runControllerWithExitExceptionAndGetContent     ('reports/default/create');
             $this->assertFalse(strpos($content, 'Rows and Columns Report') === false);
             $this->assertFalse(strpos($content, 'Summation Report') === false);
             $this->assertFalse(strpos($content, 'Matrix Report') === false);
-            
+
             $this->setGetArray(array('type' => 'RowsAndColumns'));
             $this->resetPostArray();
             $content = $this->runControllerWithNoExceptionsAndGetContent     ('reports/default/create');
             $this->assertFalse(strpos($content, 'Accounts') === false);
-            
-            $this->setGetArray(array('type' => 'RowsAndColumns'));                                                                                
+
+            $this->setGetArray(array('type' => 'RowsAndColumns'));
             $this->setPostArray(array(
                                     'validationScenario' => 'ValidateForDisplayAttributes',
                                     'RowsAndColumnsReportWizardForm' => array(
@@ -79,7 +89,7 @@
                                                                'availableAtRunTime' => '0')),
                                         'filtersStructure' => '1',
                                         'displayAttributes' => '',
-                                        'DisplayAttributes' => array(   
+                                        'DisplayAttributes' => array(
                                                                 '0' => array(
                                                                         'attributeIndexOrDerivedType' => 'name',
                                                                         'label' => 'Name')),
@@ -98,7 +108,7 @@
                                         'OrderBysRowCounter' => '0',
                                         'save' => 'save',
                                         'ajax' => 'edit-form'
-                                    ));            
+                                    ));
             $content = $this->runControllerWithExitExceptionAndGetContent     ('reports/default/save');
             //todo: confirm validated, then continue with save
         }
@@ -126,7 +136,7 @@
             //go to details
             //delete
         }
-        
+
         /*
         * @depends on testCreateAction()
         */
@@ -135,12 +145,12 @@
             //todo:
             return;
           $super                    = $this->logoutCurrentUserLoginNewUserAndGetByUsername('super');
-          $savedReport              = SavedReport::getByName('DJTCD');          
+          $savedReport              = SavedReport::getByName('DJTCD');
           $savedReport              = SavedReport::getById((int)$savedReport->id);
           $report                   = SavedReportToReportAdapter::makeReportBySavedReport($savedReport);
           $stickySearchKey          = null;
-          $dataProvider             = $this->getDataProviderForExport($report,$stickySearchKey,false);            
-          $totalItems               = intval($dataProvider->calculateTotalItemCount());  
+          $dataProvider             = $this->getDataProviderForExport($report,$stickySearchKey,false);
+          $totalItems               = intval($dataProvider->calculateTotalItemCount());
           if($totalItems > ExportModule::$asynchronusThreshold)
           {
             $this->setGetArray(array('id' => $savedReport->id));
