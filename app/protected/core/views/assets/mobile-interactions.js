05b0fd760f7d4171773645ce6e90054f70cf1aae
Amit Ashckenazi <obviocity@gmail.com>
2013-04-18 19:16:32 -0300

    mobile main menu enhancements

--- app/protected/core/views/assets/mobile-interactions.js
+++ app/protected/core/views/assets/mobile-interactions.js
@@ -36,13 +36,15 @@
 $(window).ready( function(){
 	$('#nav-trigger').click(
 		function(e){
-			e.preventDefault();
+            var timeToClass = 0;
             if ( $('.AppContent').hasClass('nav-open') ){
                 $('.AppNavigation').removeClass('high-z-index');
-                $('#MenuView .nav').children().find('> div, > ul').fadeOut(100);
+                timeToClass = 230;
             }
             $('.AppContent').toggleClass('nav-open');
-			return false;
+            setTimeout(function() { $('.AppNavigation').toggleClass('nav-open'); }, timeToClass);
+            e.preventDefault();
+            return false;
 		}
 	);
     $('.mobile-flyout-trigger').click(
