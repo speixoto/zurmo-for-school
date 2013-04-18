88c4f7caf442881e33be8f76e1792d747ca34e15
Amit Ashckenazi <obviocity@gmail.com>
2013-04-18 18:45:22 -0300

    misc mobile fixes

--- app/protected/core/views/assets/mobile-interactions.js
+++ app/protected/core/views/assets/mobile-interactions.js
@@ -53,3 +53,9 @@ $(window).ready( function(){
         }
     );
 });
+
+$(document).on('focus', 'input, textarea', function() {
+    setTimeout(function() {
+        window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+    }, 0);
+});
