13456635254cc169dde585a2f7e88f70e7c5f5e4
Amit Ashckenazi <obviocity@gmail.com>
2013-04-19 15:55:02 -0300

    user profile fix. canceling floating save/cancel in mobile.

--- app/protected/core/views/assets/interactions.js
+++ app/protected/core/views/assets/interactions.js
@@ -199,14 +199,16 @@ $(window).ready(function(){
 var windowTop, diff;
 
 function dockFloatingBar(){
-    if ($('.float-bar').find('.disable-float-bar').length == 0) {
-        windowTop = $(window).scrollTop();
-        diff = $(document).height() - $(window).height() - 100; //100px is to dock it before scrolling all the way to the bottom
-        if( windowTop > diff ) {
-            $('.float-bar .view-toolbar-container').addClass('dock');
-        } else {
-            if( $('.wrapper').height() > $('.AppNavigation').height() ) {
-                $('.float-bar .view-toolbar-container').removeClass('dock');
+    if ($('body').hasClass('mobile-app') === false){
+        if ($('.float-bar').find('.disable-float-bar').length == 0) {
+            windowTop = $(window).scrollTop();
+            diff = $(document).height() - $(window).height() - 100; //100px is to dock it before scrolling all the way to the bottom
+            if( windowTop > diff ) {
+                $('.float-bar .view-toolbar-container').addClass('dock');
+            } else {
+                if( $('.wrapper').height() > $('.AppNavigation').height() ) {
+                    $('.float-bar .view-toolbar-container').removeClass('dock');
+                }
             }
         }
     }
