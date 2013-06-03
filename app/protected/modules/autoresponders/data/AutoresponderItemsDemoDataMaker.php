f87a10c606dbfe86edb85264dd4adae4e459c206
digimagic <none@none>
2013-06-02 22:15:07 -0500

    Enriching marketing demo data

--- app/protected/modules/autoresponders/data/AutoresponderItemsDemoDataMaker.php
+++ app/protected/modules/autoresponders/data/AutoresponderItemsDemoDataMaker.php
@@ -43,7 +43,7 @@
 
         public static function getDependencies()
         {
-            return array('contacts');
+            return array('contacts', 'marketingLists');
         }
 
         public function makeAll(& $demoDataHelper)
