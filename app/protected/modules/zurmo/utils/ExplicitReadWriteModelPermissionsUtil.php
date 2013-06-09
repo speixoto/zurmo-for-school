53011bf897147c1da1b92b90a029cdb91eafb39b
digimagic <none@none>
2013-06-08 20:24:56 -0500

    Tweaking permissions to allow non-created models to be fully accessible by the created user.

--- app/protected/modules/zurmo/utils/ExplicitReadWriteModelPermissionsUtil.php
+++ app/protected/modules/zurmo/utils/ExplicitReadWriteModelPermissionsUtil.php
@@ -356,7 +356,6 @@
                                ExplicitReadWriteModelPermissions $explicitReadWriteModelPermissions)
         {
             assert('$securableItem->id < 0');
-            $securableItem->setTreatCreatedByUserAsOwnerForPermissions(true);
             if ($explicitReadWriteModelPermissions->getReadOnlyPermitablesCount() > 0)
             {
                 foreach ($explicitReadWriteModelPermissions->getReadOnlyPermitables() as $permitable)
@@ -371,7 +370,6 @@
                     $securableItem->addPermissions($permitable, Permission::READ_WRITE_CHANGE_PERMISSIONS_CHANGE_OWNER);
                 }
             }
-            $securableItem->setTreatCreatedByUserAsOwnerForPermissions(false);
             return true;
         }
 
