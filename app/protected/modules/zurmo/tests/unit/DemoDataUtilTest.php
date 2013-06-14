ad0ba4a649771bc3e458934a6e6ec07aacf70242
Muhammad Shoaib <shoaibi@dotgeek.me>
2013-06-14 22:02:10 +0500

    Moved profiling related settings to separate xml config

--- app/protected/modules/zurmo/tests/unit/DemoDataUtilTest.php
+++ app/protected/modules/zurmo/tests/unit/DemoDataUtilTest.php
@@ -51,7 +51,6 @@
 
         public function testLoad()
         {
-            //return;//temporarily disabled
             $this->assertEquals(2, Group::getCount());
             $this->assertEquals(0, Role::getCount());
             $this->assertEquals(0, Account::getCount());
