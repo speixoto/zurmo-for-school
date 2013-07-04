7747f79540160d32f9120f265a741137bc3f1d1d
msinghai <none@none>
2013-07-04 14:38:38 +0530

    Fix for story 52844567

--- app/protected/modules/products/controllers/DefaultController.php
+++ app/protected/modules/products/controllers/DefaultController.php
@@ -334,14 +334,15 @@
             assert('$id != null && $id != ""');
             assert('$value != null && $value != ""');
             $id         = intval($id);
-            $value      = intval($value);
             $product    = Product::getById($id);
             ControllerSecurityUtil::resolveAccessCanCurrentUserWriteModel($product);
             switch($attribute)
             {
-                case 'quantity'     : $product->quantity     = $value;
+                case 'quantity'     :   $value      = intval($value);
+                                        $product->quantity     = $value;
                                         break;
-                case 'sellPrice'    : $product->sellPrice->value = $value;
+                case 'sellPrice'    :   $value      = floatval($value);
+                                        $product->sellPrice->value = $value;
                                         break;
             }
             $product->save();
@@ -380,6 +381,11 @@
             $product->priceFrequency    = $productTemplate->priceFrequency;
             $product->sellPrice         = $sellPrice;
             $product->type              = $productTemplate->type;
+
+            foreach($productTemplate->productCategories as $productCategory)
+            {
+                $product->productCategories->add($productCategory);
+            }
 
             $relationModel              = $relationModelClassName::getById((int)$relationModelId);
             $product->$relationAttributeName = $relationModel;
