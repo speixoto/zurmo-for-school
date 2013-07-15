deaa5eac496c9d5273c179e683f87cbff5b305b3
msinghai <none@none>
2013-07-15 23:52:49 +0530

    Clean up

--- app/protected/modules/products/elements/actions/ProductEditLinkActionElement.php
+++ app/protected/modules/products/elements/actions/ProductEditLinkActionElement.php
@@ -30,11 +30,5 @@
         {
             return Yii::app()->createUrl($this->params['relationModuleId'] . '/default/details?id=' . $this->params['relationModelId']);
         }
-
-        protected function getDefaultRoute()
-        {
-            $params = array('id' => $this->modelId, 'redirectUrl' => $this->getRedirectUrl());
-            return Yii::app()->createUrl($this->moduleId . '/' . $this->controllerId . '/edit/', $params);
-        }
     }
 ?>
