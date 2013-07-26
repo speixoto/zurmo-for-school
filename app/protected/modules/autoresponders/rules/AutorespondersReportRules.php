f1b67452f20ed097b5c3be393e8a9775cdd749c8
digimagic <none@none>
2013-07-26 11:15:27 -0500

    Refining marketing list report rules and autoresponder report rules

--- app/protected/modules/autoresponders/rules/AutorespondersReportRules.php
+++ app/protected/modules/autoresponders/rules/AutorespondersReportRules.php
@@ -46,8 +46,10 @@
         {
             $metadata = array(
                 'Autoresponder' => array(
-                    'nonReportable' =>
-                    array(),
+                    'nonReportable' => array('files'),
+                ),
+                'AutoresponderItem' => array(
+                    'nonReportable' => array('autoresponderItemActivities', 'emailMessage'),
                 )
             );
             return array_merge(parent::getDefaultMetadata(), $metadata);
