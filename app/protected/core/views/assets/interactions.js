afa8723ffb169c12c33a7d8a931b0c781da956c0
Sérgio Peixoto <matematico2002@hotmail.com>
2013-09-18 20:06:00 +0100

    Clickable-dividedmenu should stay opened on hover and only close on click.
    --HG--
    branch : userInterface

--- app/protected/core/views/assets/interactions.js
+++ app/protected/core/views/assets/interactions.js
@@ -678,6 +678,14 @@ $(window).ready(function(){
         }
     );
 
+    $('.clickable-dividedmenu').find('a.button-trigger, a.button-action-trigger').hover(
+        function(){
+            if ( $(this).parent().hasClass('nav-open') === false ){
+                $(this).parent().addClass('nav-open');
+            }
+        }
+    );
+
     //Main nav hover
      $('#MenuView > ul > li > a, #RecentlyViewedView  > ul > li > a').hover(
         function(){
