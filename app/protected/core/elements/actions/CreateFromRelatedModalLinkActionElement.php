423b722244b74f92e9ebbc1e99e4c0439074a20d
msinghai <none@none>
2013-10-23 22:07:24 +0530

    Fixes for kanban view, refresh issue and zero model issue
    --HG--
    branch : next-default

--- app/protected/core/elements/actions/CreateFromRelatedModalLinkActionElement.php
+++ app/protected/core/elements/actions/CreateFromRelatedModalLinkActionElement.php
@@ -83,9 +83,10 @@
          */
         protected function getCreateLinkUrlParams()
         {
-            return array(
-                'modalTransferInformation' => $this->getModalTransferInformation(),
-            );
+            return $this->getModalTransferInformation();
+//            return array(
+//                'modalTransferInformation' => $this->getModalTransferInformation(),
+//            );
         }
 
         /**
