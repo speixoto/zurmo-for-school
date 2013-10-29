5942f77b00a7a55e48e4fce27b5a9503338f562b
digimagic <none@none>
2013-10-29 10:35:35 -0500

    Removing coin completely after clicking it

--- app/protected/modules/gamification/views/GameCoinContainerView.php
+++ app/protected/modules/gamification/views/GameCoinContainerView.php
@@ -78,7 +78,6 @@
             $url    = $this->makeAjaxClickUrl();
             $script = "$('.random-game-coin').click(function(){
                             " . ZurmoHtml::ajax(array('type' => 'GET', 'url' =>  $url)) . "
-                            //$(this).animate( { left:1200, top:300 }, 35, 'easeOutQuart' );
                             var audio = document.getElementById('game-coin-chime');
                             audio.play();
                             $('.game-coin').animate({top:15},75).hide(0);
@@ -87,6 +86,9 @@
                                 totalFrames: 40,
                                 duration: 1000,
                                 loop: false,
+                                complete: function(){
+                                    $('.random-game-coin').remove();
+                                }
                             });
                         });";
            Yii::app()->clientScript->registerScript('gameCoinClickScript', $script);
