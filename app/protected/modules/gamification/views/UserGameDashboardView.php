2fa5c8bb0176271cfaa5617d68f5aa823425759a
Amit Ashckenazi <obviocity@gmail.com>
2013-10-31 20:46:28 -0300

    responsive game dashboard and misc tweaks

--- app/protected/modules/gamification/views/UserGameDashboardView.php
+++ app/protected/modules/gamification/views/UserGameDashboardView.php
@@ -142,7 +142,7 @@
 
         protected function registerCloseButtonScript()
         {
-            $script = "$('.close-dashboard-button a').on('click', function(){
+            $script = "$('.close-dashboard-button a, #gd-overlay, #gd-container, #gd-centralizer').on('click', function(){
                            if($('#UserGameDashboardView').length){
                                closeGamificationDashboard();
                                return false;
@@ -302,7 +302,7 @@ SPT;
                 $rankingContent .= ZurmoHtml::tag('span', array(), $ranking['typeLabel']);
                 $content .= ZurmoHtml::tag('div', array('class' => 'leaderboard-rank'), $rankingContent);
             }
-            return      ZurmoHtml::tag('div', array('id' => 'gd-leaderboard'), $content);
+            return      ZurmoHtml::tag('div', array('id' => 'gd-leaderboard', 'class' => 'clearfix'), $content);
         }
 
         protected function renderStatisticsContent()
