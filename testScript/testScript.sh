3e278449d15fdc4dc8e689e58e10d3419086911d
Sérgio Peixoto <matematico2002@hotmail.com>
2013-11-08 19:30:22 +0000

    Fixing testScript.sh

--- testScript/testScript.sh
+++ testScript/testScript.sh
@@ -5,8 +5,8 @@ chmod -R 757 app/protected/data
 chmod -R 757 app/protected/runtime
 chmod -R 757 app/assets
 chmod -R 757 app/protected/config
-cp debugTest.php app/protected/config/debugTest.php
-cp perInstanceTest.php app/protected/config/perInstanceTest.php
+cp testScript/debugTest.php app/protected/config/debugTest.php
+cp testScript/perInstanceTest.php app/protected/config/perInstanceTest.php
 chmod -R 757 app/protected/config/debugTest.php
 chmod -R 757 app/protected/config/perInstanceTest.php
 chmod -R 757 app/version.php
