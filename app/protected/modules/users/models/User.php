0f5175ba96e62fb331b6e1d1f3a0d269be5b94bb
matematico2002@hotmail.com <matematico2002@hotmail.com>
2013-11-25 17:35:03 +0000

    Fixing password not behing required in users import (#60778248)
    --HG--
    branch : next-default

--- app/protected/modules/users/models/User.php
+++ app/protected/modules/users/models/User.php
@@ -724,6 +724,7 @@
                 ),
                 'rules' => array(
                     array('hash',     'type',    'type' => 'string'),
+                    array('hash',     'required'),
                     array('hash',     'length',  'min'   => 32, 'max' => 32),
                     array('language', 'type',    'type'  => 'string'),
                     array('language', 'length',  'max'   => 10),
