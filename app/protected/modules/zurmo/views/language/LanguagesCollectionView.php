87e2d173f3a7aa1c923491cd9d21956c75bb1267
Sérgio Peixoto <matematico2002@hotmail.com>
2014-01-21 23:15:19 +0000

    Removing deactivateButton when language is in use (#56367396)

--- app/protected/modules/zurmo/views/language/LanguagesCollectionView.php
+++ app/protected/modules/zurmo/views/language/LanguagesCollectionView.php
@@ -190,6 +190,10 @@ EOD;
 
                 if ($languageCode != Yii::app()->sourceLanguage)
                 {
+                    if (!Yii::app()->languageHelper->canDeactivateLanguage($languageCode))
+                    {
+                        $content .= ' - ' . Zurmo::t('ZurmoModule', 'Language in use.');
+                    }
                     $content .= $this->renderUpdateButton($languageCode, $languageData);
                     $content .= $this->renderDeactivateButton($languageCode, $languageData);
                 }
@@ -223,6 +227,10 @@ EOD;
         {
             assert('is_string($languageCode)');
             assert('is_array($languageData)');
+            if (!Yii::app()->languageHelper->canDeactivateLanguage($languageCode))
+            {
+                return;
+            }
             return ZurmoHtml::link(
                 $this->renderButtonSpinnerSpans() .
                 ZurmoHtml::tag(
