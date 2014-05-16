eb3a367d955e2fe1ff31d4609a2b1d00fe36ca21
Sushil Meher <none@none>
2014-05-16 19:10:05 +0530

    fixing PhpUnitServiceUtil for version 4.0

--- app/protected/tests/common/PhpUnitServiceUtil.php
+++ app/protected/tests/common/PhpUnitServiceUtil.php
@@ -45,22 +45,8 @@
         {
             try
             {
-                // For PHPUnit < 3.7
-                if (is_file('PHPUnit/Runner/Version.php') && is_readable('PHPUnit/Runner/Version.php'))
-                {
-                    include_once 'PHPUnit/Runner/Version.php';
-                    $actualVersion = PHPUnit_Runner_Version::id();
-                }
-                // For PHPUnit > 3.7
-                elseif (method_exists('PHPUnit_Runner_Version', 'id'))
-                {
-                    $actualVersion = PHPUnit_Runner_Version::id();
-                }
-                else
-                {
-                    // PHPUnit probably is not installed.
-                    throw new Exception();
-                }
+                $actualVersion = shell_exec('phpunit --version');
+                $actualVersion = substr($actualVersion, 8, 6);
 
                 if (version_compare($actualVersion, self::$phpUnitMinimumVersion) < 0)
                 {
