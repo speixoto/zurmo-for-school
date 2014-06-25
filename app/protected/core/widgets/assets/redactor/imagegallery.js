109f85a5f61b9db7753d4df132988ed32b44569d
Amit Ashckenazi <obviocity@gmail.com>
2014-06-25 11:26:39 -0300

    redactor image plugin/modal fixes
    --HG--
    branch : imageUploadAndGalleryV2

--- app/protected/core/widgets/assets/redactor/imagegallery.js
+++ app/protected/core/widgets/assets/redactor/imagegallery.js
@@ -18,15 +18,14 @@ RedactorPlugins.imagegallery = {
                 $('#redactor_modal_inner').empty().append(data);
                 $('#redactor_modal').off('click', linkForInsertSelector);
                 $('#redactor_modal').on('click', linkForInsertSelector, callback);
-                $('#redactor_modal').addClass('ui-dialog redactor-image-modal');
             },
             error: function (xhr, status) {
                 alert("Sorry, there was a problem!");
             }
         });
-
         this.modalInit(this.opts.curLang.image, '', 800);
-
+        this.$modal.addClass('ui-dialog redactor-image-modal');
+        this.$modal.animate({top:'41', left:'35%'}, 50);
     },
     insertFromGalleryModal: function(event)
     {
