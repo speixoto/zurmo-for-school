f4bdd680300ec9bb21d84e257c1d33a52cdbce30
popoye <none@none>
2014-07-14 19:47:35 +0530

    reverting changes.
    --HG--
    branch : next-default

--- app/protected/modules/marketing/adapters/MarketingConfigurationFormAdapter.php
+++ app/protected/modules/marketing/adapters/MarketingConfigurationFormAdapter.php
@@ -47,6 +47,7 @@
         public static function makeFormFromMarketingConfiguration()
         {
             $form                                         = new MarketingConfigurationForm();
+            $form->autoresponderOrCampaignBatchSize       = AutoresponderOrCampaignBatchSizeConfigUtil::getBatchSize();
             $form->autoresponderOrCampaignFooterPlainText = GlobalMarketingFooterUtil::getContentByType(false);
             $form->autoresponderOrCampaignFooterRichText  = GlobalMarketingFooterUtil::getContentByType(true);
             return $form;
@@ -57,6 +58,10 @@
          */
         public static function setConfigurationFromForm(MarketingConfigurationForm $form)
         {
+            if (Yii::app()->user->userModel->isRootUser)
+            {
+                AutoresponderOrCampaignBatchSizeConfigUtil::setBatchSize((int)$form->autoresponderOrCampaignBatchSize);
+            }
             GlobalMarketingFooterUtil::setContentByType($form->autoresponderOrCampaignFooterPlainText, false);
             GlobalMarketingFooterUtil::setContentByType($form->autoresponderOrCampaignFooterRichText, true);
         }
