9b94696424f37566badf4dde9420b33fec3c8b11
digimagic <none@none>
2014-07-14 12:59:21 -0500

    fixing default permission settings for new performance work. leaving off by default if you have tested groups/roles consider turning processNamedSecurableActualPermissionsAsNonOptimized and processReadMungeAsWriteMunge on
    --HG--
    branch : next-default

--- app/protected/config/common.php
+++ app/protected/config/common.php
@@ -457,8 +457,8 @@
                 'de' => 'German',
             ),
             'sentryDsn'    => 'http://5232100222bc4404b368026413df2d9a:47f7a2f1542348d68bea7b00f2261ede@sentry.zurmo.com/2',
-            'processNamedSecurableActualPermissionsAsOptimized' => true,
-            'processReadMungeAsWriteMunge' => true,
+            'processNamedSecurableActualPermissionsAsNonOptimized' => false,
+            'processReadMungeAsWriteMunge' => false,
             'showFlashMessageWhenSecurityCacheShouldBeRebuilt' => false,
         ),
         'preload'                       => array(
