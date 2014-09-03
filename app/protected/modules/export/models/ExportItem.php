01f5906346f35c97e9f8c5621098e82c3717ac7d
Muhammad Shoaib <shoaibi@dotgeek.me>
2014-09-03 21:12:00 +0500

    [Delivered #77455936] changed serializeData type to longtext

--- app/protected/modules/export/models/ExportItem.php
+++ app/protected/modules/export/models/ExportItem.php
@@ -84,8 +84,9 @@
                     array('processOffset',    'type',    'type' => 'integer'),
                     array('processOffset',    'default',    'value' => 0),
                     array('serializedData',   'required'),
-                    array('serializedData',   'type', 'type' => 'string'),
+                    array('serializedData',   'type', 'type' => 'longtext'),
                     //4,294,967,295 is the max storage capacity for longtext
+                    // keeping this rule here for safe-keeping
                     array('serializedData',   'length', 'max' => 4294967290),
                     array('isJobRunning',     'boolean'),
                     array('cancelExport',     'boolean'),
